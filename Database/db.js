22c474d3e2fbca91651fb91d6a96d9f027c81608
EduardoMB14 <163693042+EduardoMB14@users.noreply.github.com>
2024-06-19 22:25:40 -0600

    Update db.js
    update host

--- Database/db.js
+++ Database/db.js
@@ -1,6 +1,6 @@
 const mysql = require('mysql');
 const connection = mysql.createConnection({
-    host: process.env.MYSQL_ADDON_URI,
+    host: process.env.MYSQL_ADDON_HOST,
     user: process.env.MYSQL_ADDON_USER,
     password: process.env.MYSQL_ADDON_PASSWORD,
     database: process.env.MYSQL_ADDON_DB
